8610108e85d3c8359e1e07dee699c1239498c745
Leonardo <leonardo.otero390@gmail.com>
2021-08-07 18:21:14 -0400

    Ask name and verify to enter room

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,53 @@
-// function enterRoom () {
-// const userName = "yas" ;//prompt("Diga seu nome");
-// const request = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants', {name: userName});
+let clientName, messages, messagesCounter;
+function enterRoom() {
+    if (clientName !== undefined) {
+        clientName = prompt("Este nome já está em uso, por favor escolha outro:");
+    } else if (clientName === undefined) {
+        clientName = prompt("Diga seu nome");
+    }
+    const request = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants', { name: clientName });
 
-// request.then(function (param){
-//     console.log(param.status);
-// })
-// request.catch(function (error){
-//         console.log(error.response.status);
-//     });
-// }
-// enterRoom();
-const clientName = "Leo";
-let messages;
-let messagesCounter;
+    request.catch(enterRoom);
+}
+
 function getMessages(response) {
     messages = response.data;
-    document.querySelector(".chat-box" ).innerHTML = "";
+    document.querySelector(".chat-box").innerHTML = "";
     renderMessages();
     messagesCounter.toString();
-    let lastMessage = document.querySelector("#msg-id-"+messagesCounter);
+    let lastMessage = document.querySelector("#msg-id-" + messagesCounter);
     lastMessage.scrollIntoView();
 }
 function loadMessages() {
-    
+
     const promise = axios.get(
         "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages"
     );
     promise.then(getMessages);
 }
 function renderMessages() {
-    for (let i = 0; i<messages.length; i++) {
+    for (let i = 0; i < messages.length; i++) {
         messagesCounter = i;
-    let msgType = messages[i].type;
-    let destinyText;
-    if (msgType === "status") {
-        destinyText = "";
-        messages[i].to = "";
-    } else if (msgType === "message") {
-        destinyText = "para";
-    } else {
-        if (messages[i].to !== clientName && messages[i].from !== clientName) continue;
-        destinyText = "resevadamente para";
-    }
-    document.querySelector(".chat-box" ).innerHTML += `<div class="${msgType} chat-status" id ="msg-id-${i}">
+        let msgType = messages[i].type;
+        let destinyText;
+        if (msgType === "status") {
+            destinyText = "";
+            messages[i].to = "";
+        } else if (msgType === "message") {
+            destinyText = "para";
+        } else {
+            if (messages[i].to !== clientName && messages[i].from !== clientName) continue;
+            destinyText = "resevadamente para";
+        }
+        document.querySelector(".chat-box").innerHTML += `<div class="${msgType} chat-status" id ="msg-id-${i}">
     <p class="message-content">
       <time>(${messages[i].time})</time>
      <strong> ${messages[i].from}</strong> ${destinyText} <strong>${messages[i].to}</strong> ${messages[i].text}
     </p>
   </div>`;
-  
+
+    }
 }
-}
+enterRoom();
 loadMessages();
-setInterval(loadMessages,3000);
+setInterval(loadMessages, 3000);
