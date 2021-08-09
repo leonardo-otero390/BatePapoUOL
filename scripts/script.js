01f1c29afbd05ef210c7f0a07747339b2b155f70
Leonardo <leonardo.otero390@gmail.com>
2021-08-09 10:43:46 -0400

    Loading participants and sending private messages

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,12 @@ const URL_API_UOL = {
     status: "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status",
     messages: "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages"
 }
-let clientName, messages, messagesCounter;
+const SECONDS = 1000;
+let clientName, messages, messagesCounter, participants;
+let userRecipient = "Todos";
+let visibility = "message";
+let alertVisibility = "";
+
 function enterRoom() {
     if (clientName !== undefined) {
         clientName = prompt("Este nome já está em uso, por favor escolha outro:");
@@ -13,12 +18,12 @@ function enterRoom() {
     const request = axios.post(URL_API_UOL.participants, { name: clientName });
 
     request.catch(enterRoom);
-    request.then(setInterval(keepConnection, 5000));
+    request.then(setInterval(keepConnection, 5 * SECONDS));
 }
 function keepConnection() {
     axios.post(URL_API_UOL.status, { name: clientName });
 }
-function getMessages(response) {
+function loadMessages(response) {
     messages = response.data;
     document.querySelector(".chat-box").innerHTML = "";
     renderMessages();
@@ -26,12 +31,10 @@ function getMessages(response) {
     let lastMessage = document.querySelector("#msg-id-" + messagesCounter);
     lastMessage.scrollIntoView();
 }
-function loadMessages() {
+function getMessages() {
 
-    const promise = axios.get(
-        URL_API_UOL.messages
-    );
-    promise.then(getMessages);
+    const promise = axios.get(URL_API_UOL.messages);
+    promise.then(loadMessages);
 }
 function renderMessages() {
     for (let i = 0; i < messages.length; i++) {
@@ -56,19 +59,22 @@ function renderMessages() {
 
     }
 }
+function treatSendError(error) {
+    console.log(error.message);
+    //window.location.reload();
+}
 function sendMessage() {
     let message = document.querySelector(".type-message").value;
+    console.log(visibility);
     const request = axios.post(URL_API_UOL.messages, {
         from: clientName,
-        to: "Todos",
+        to: userRecipient,
         text: message,
-        type: "message"
+        type: visibility
     });
 
-    request.then(loadMessages);
-    request.catch(function () {
-        window.location.reload();
-    });
+    request.then(getMessages);
+    request.catch(treatSendError);
     document.querySelector(".type-message").value = "";
 }
 function showSidebar() {
@@ -81,6 +87,57 @@ function hideSidebar() {
     element[0].classList.add("hidden");
     element[1].classList.add("hidden");
 }
+function alertText () {
+    if (visibility === "private_message") {
+        alertVisibility = "(reservadamente)";
+    } else alertVisibility = "";
+    document.querySelector(".alert-send-message").innerHTML = `Enviando para ${userRecipient} ${alertVisibility}`;
+}
+function selectUser(element) {
+    if (document.querySelector(".users-list .check.marked") !== null) {
+        document.querySelector(".users-list .check.marked").classList.remove("marked");
+    }
+    element.querySelector(".check").classList.add("marked");
+    userRecipient = element.querySelector(".user").innerText;
+    alertText();
+}
+function selectVisibility(element) {
+    if (document.querySelector(".visibility .check.marked") !== null) {
+        document.querySelector(".visibility .check.marked").classList.remove("marked");
+    }
+    element.querySelector(".check").classList.add("marked");
+    visibility = element.classList[1];
+    alertText();
+}
+function renderParticipants(element) {
+    element.innerHTML = `<div class="user-option" onclick = "selectUser(this)">
+    <div class="user">
+      <ion-icon name="people-sharp"></ion-icon>
+      Todos
+    </div>
+    <ion-icon name="checkmark-sharp" class="check"></ion-icon>
+  </div>`
+    for (let i = 0; i < participants.length; i++) {
+        element.innerHTML += `<div class="user-option" onclick = "selectUser(this)">
+        <div class="user">
+          <ion-icon name="person-circle-sharp"></ion-icon>
+          ${participants[i].name}
+        </div>
+        <ion-icon name="checkmark-sharp" class="check"></ion-icon>
+      </div>`
+    }
+}
+function loadParticipants(response) {
+    participants = response.data;
+    const element = document.querySelector(".users-list");
+    renderParticipants(element);
+}
+function getParticipants() {
+    const promise = axios.get(URL_API_UOL.participants);
+    promise.then(loadParticipants);
+}
 enterRoom();
-loadMessages();
-setInterval(loadMessages, 3000);
+getMessages();
+getParticipants();
+setInterval(getMessages, 3 * SECONDS);
+setInterval(getParticipants, 10 * SECONDS);
